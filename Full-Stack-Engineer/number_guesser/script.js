a5ca24512c376280ae8234431006346084d4fbd0
VeryExcitedGremlin <102506719+VeryExcitedGremlin@users.noreply.github.com>
2026-05-13 18:32:38 -0600

    Update script.js

--- Full-Stack-Engineer/number_guesser/script.js
+++ Full-Stack-Engineer/number_guesser/script.js
@@ -4,3 +4,44 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    return Math.floor(Math.random() * 10);
+};
+
+function compareGuesses(userGuess, comGuess, target) {
+    if (userGuess < 0 || userGuess > 9) {
+        alert('Target number will be between 0 and 9');
+    };
+
+    const userClose = getAbsolute(userGuess, target);
+    const comClose = getAbsolute(comGuess, target);
+
+    if (userClose <= comClose) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+function getAbsolute(guess, target) {
+    return Math.abs(guess - target);
+};
+
+function updateScore(winner) {
+    if (winner === 'human') {
+        humanScore ++;
+    } else {
+        computerScore ++;
+    };
+};
+
+function advanceRound() {
+    currentRoundNumber ++
+}
+
+
+
+
+
+
+
